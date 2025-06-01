3457fe238d7f7cbd5536f9c98452272f55e6e472
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2025-06-01 22:24:14 +0800

    feat: 在RagService中添加minScore参数以优化查询结果

--- src/modules/agent/mastra/rag/index.ts
+++ src/modules/agent/mastra/rag/index.ts
@@ -35,6 +35,7 @@ export class RagService {
       indexName: this.indexName,
       queryVector: embedding,
       topK: 4,
+      minScore: 0.75,
     });
     this.logger.log("查询结果");
     this.logger.log(result)
